8ea282e271600242217070042fd69aca34727a66
hanzalahsamana <hanzalahsamana789@gmail.com>
2024-10-18 19:49:17 +0500

    solve naming convention

--- src/components/sections/productsSection.jsx
+++ src/components/sections/productsSection.jsx
@@ -5,6 +5,7 @@ import { fetchProducts } from '@/Apis/FetchProducts';
 import { setProductData, setLoading, setError } from '@/Redux/Products/productDataSlice';
 import { useRouter } from 'next/navigation';
 import ProductCard from '../UI/productCard';
+import Link from 'next/link';
 
 const ProductsSection = ({ maxLength, collection , name }) => {
     console.log(collection);
@@ -27,13 +28,13 @@ const ProductsSection = ({ maxLength, collection , name }) => {
             <div className="grid grid-cols-4 max-[1024px]:grid-cols-3 max-[750px]:grid-cols-2 max-[470px]:grid-cols-1 gap-2 m-6">
                 {products?.slice(0, maxLength).map((product) => (
                     (product?.collectionName?.toLowerCase() === collection || collection === "all") && (
-                        <div
+                        <Link
                             className="cursor-pointer"
                             key={product._id}
-                            onClick={() => router.push(`products/${product._id}`)}
+                            href={`products/${product._id}`}
                         >
                             <ProductCard product={product} />
-                        </div>
+                        </Link>
                     )
                 ))}
             </div>
